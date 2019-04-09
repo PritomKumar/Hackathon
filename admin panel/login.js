fd951b087b20df1c654bc2d370d9f0ee2be08a53
PritomKumar <bsse0919@iit.du.ac.bd>
2019-04-09 16:52:57 -0700

    Admin Panel part 2

---
+++ admin panel/login.js
@@ -0,0 +1,41 @@
+function toggleResetPswd(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle() // display:block or none
+    $('#logreg-forms .form-reset').toggle() // display:block or none
+}
+
+function toggleSignUp(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle(); // display:block or none
+    $('#logreg-forms .form-signup').toggle(); // display:block or none
+}
+
+$(()=>{
+    // Login Register Form
+    $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+    $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+    $('#logreg-forms #btn-signup').click(toggleSignUp);
+    $('#logreg-forms #cancel_signup').click(toggleSignUp);
+})
+
+$('#google-button').on('click', function() {
+  // Initialize with your OAuth.io app public key
+  OAuth.initialize('YOUR_OAUTH_KEY');
+  // Use popup for oauth
+  OAuth.popup('google').then(google => {
+    console.log('google:',google);
+    // Retrieves user data from oauth provider
+    // Prompts 'welcome' message with User's email on successful login
+    // #me() is a convenient method to retrieve user data without requiring you
+    // to know which OAuth provider url to call
+    google.me().then(data => {
+      console.log('me data:', data);
+      alert('Google says your email is:' + data.email + ".\nView browser 'Console Log' for more details");
+    });
+    // Retrieves user data from OAuth provider by using #get() and
+    // OAuth provider url
+    google.get('/plus/v1/people/me').then(data => {
+      console.log('self data:', data);
+    })
+  });
+})

92897238dd5e81007874877ca0088a6908501f1b
PritomKumar <bsse0919@iit.du.ac.bd>
2019-04-09 17:53:59 -0700

    bug fix

--- admin panel/logIn.php
+++ admin panel/logIn.php
@@ -12,7 +12,7 @@
 
 
     $result = mysql_query("select * from users where username= '$username' and password ='$password'" )
-                or die ("Failed to Query Database", mysql_error());
+                or die ("Failed to Query Database",mysql_error());
 
     $row = mysql_fetch_array($result);
     if($row['username'] == $username && $row['password'] == $passowrd){
